b4383ace91f0870ac138985345b71d43a6a20583
0-Chan <kyc4421194@gmail.com>
2022-03-10 08:43:57 +0900

    Adds fastapi sqlite tutorial

--- api-server/main.py
+++ api-server/main.py
@@ -1,7 +1,14 @@
 from typing import Optional
 
-from fastapi import FastAPI, Depends
-from user import get_user
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -13,10 +20,65 @@ def read_root():
 async def hello():
     return {"msg":"Hello, this is API server"}
 
-@app.get("/api/me")
-async def hello_user(user = Depends(get_user)):
-    return {"msg":"Hello, user","uid":user['uid']} 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+class Contact(BaseModel):
+  contact_id:int
+  first_name:str
+  last_name:str
+  user_name:str
+  password:str
+
+class ContactOut(BaseModel):
+    contact_id:int
+    first_name:str
+    last_name:str
+    user_name:str
+
+# @app.post('/contact')
+# async def create_contact(contact: Contact):
+#     return contact
+
+@app.post('/contact', response_model=ContactOut)
+async def create_contact(contact: Contact):
+    return contact
+
+
+    # Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=Optional[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
