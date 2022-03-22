334b06fb4e0a5a2913d8bc4b39706edb18f8c882
0-Chan <kyc4421194@gmail.com>
2022-03-23 00:21:21 +0900

    Adds error handling

--- api-server/main.py
+++ api-server/main.py
@@ -48,7 +48,9 @@
 
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -58,6 +60,12 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 # Dependency
 def get_db():
